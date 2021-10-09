39aefa0d6cd60450174a9a28c4981c908d14890f
paulohpps <paulohpps13@gmail.com>
2021-10-08 21:56:31 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String resultado = "";
+            long contador2 = 50000;
+            for (int i = 0; i <= contador2; i++)
+            {
+                resultado += " " + i;
+            }
+            Console.WriteLine(resultado);
+        }
+    }
+}
